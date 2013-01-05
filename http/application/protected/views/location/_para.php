20b4f1f804b258f7c61cccb00cf3eded3bb3b4b3
grassroot <stillgren@gmail.com>
2013-01-05 22:19:33 +0200

    Initial commit

---
+++ http/application/protected/views/location/_para.php
@@ -0,0 +1,29 @@
+<?php
+/* @var $this ParameterController */
+/* @var $model Parameter */
+/* @var $form CActiveForm */
+?>
+
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'parameter-form',
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'value'); ?>
+                <?php //echo $form->textField($model,'value'); ?>
+<?php echo $form->dropDownList($model,'value', CHtml::listData(Location::model()->findAll(), 'id', 'location')); ?>
+		<?php echo $form->error($model,'value'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton('Save'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
